c373a54eab261bd64673d1801e7329aaa431aada
ammaar11aslam <ammaar11aslam@gmail.com>
2025-03-16 13:31:41 +1100

    Minor changes made to comments

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// Import the express module
+// Importing the express module
 const express = require('express');
 const app = express();
 
-// Set the port for the server
+// Setting the port for the server
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (e.g., HTML, CSS) from the "public" directory
+// Serving static files from the "public" directory
 app.use(express.static('public'));
 
-// Set up a basic route that serves an HTML file
+// Setting up a basic route that serves an HTML file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// Start the server
+// Starting the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
